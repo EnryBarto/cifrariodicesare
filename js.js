ac6efef44683e3be01df02a03eae9871bb47cc2e
EnyBartoK <enrico.bartocetti@gmail.com>
2020-05-26 22:43:54 +0200

    Minor changes

--- js.js
+++ js.js
@@ -11,6 +11,11 @@ function encrypt(){
 		var letter = str.charAt(i);
 		var pos = alphabet.indexOf(letter);
 		var letter_changed = alphabet.charAt(pos + n);
+		
+		if (letter == " "){
+			letter_changed = " ";
+		}
+
 		text = text + letter_changed;
 	}
 	document.getElementById("encrypted_text").value = text;
@@ -18,7 +23,7 @@ function encrypt(){
 
 function decrypt(){
 	var text = "";
-	var str = document.getElementById("notencrypted_text").value;
+	var str = document.getElementById("encrypted_text").value;
 	var n = document.getElementById("key").value * 1;
 	var char = str.split("");
 	var length = str.length;
@@ -27,7 +32,12 @@ function decrypt(){
 		var letter = str.charAt(i);
 		var pos = alphabet.indexOf(letter);
 		var letter_changed = alphabet.charAt(pos - n);
+		
+		if (letter == " "){
+			letter_changed = " ";
+		}
+
 		text = text + letter_changed;
 	}
-	document.getElementById("encrypted_text").value = text;
+	document.getElementById("notencrypted_text").value = text;
 }
